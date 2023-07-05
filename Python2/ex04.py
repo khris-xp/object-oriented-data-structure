f25d4b164645857e23da05c0728b337fc5fb2350
khris-xp <khrisbharmmano@gmail.com>
2023-07-05 19:44:18 +0700

    feat: ex04 python2

---
+++ Python2/ex04.py
@@ -0,0 +1,22 @@
+def find_base_for_21(number):
+    def convert_to_base(number, base):
+        converted_number = ""
+        while number > 0:
+            digit = number % base
+            converted_number = str(digit) + converted_number
+            number //= base
+        return converted_number
+    
+    for base in range(2, number + 1):
+        converted = convert_to_base(number, base)
+        if converted == "21" or converted == "20":
+            return base, converted
+    return None
+
+year = int(input("Enter year : "))
+base, converted = find_base_for_21(year)
+
+if base is not None:
+    print(f"saimai is just {converted}, in base {base}!")
+else:
+    print("No base found where the input is equal to 21 or 20.")
