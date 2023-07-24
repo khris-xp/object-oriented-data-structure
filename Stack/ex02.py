481df990463255fb3b83a57995fcc5ecccaa5457
khris-xp <khrisbharmmano@gmail.com>
2023-07-25 01:46:40 +0700

    refac: ex02 stack

--- Stack/ex02.py
+++ Stack/ex02.py
@@ -1,67 +1,66 @@
 class Stack:
     def __init__(self):
         self.stack = []
-
-    def push(self, item):
-        self.stack.append(item)
-
-    def pop(self):
-        if not self.is_empty():
-            return self.stack.pop()
-        
-    def is_empty(self):
-        return len(self.stack) == 0
-
+    
     def size(self):
         return len(self.stack)
-
+    
+    def is_empty(self):
+        return self.size() == 0
+    
+    def push(self, item):
+        return self.stack.append(item)
+    
+    def pop(self):
+        return self.stack.pop()
+    
 stack = Stack()
+new_list_stack = Stack()
 
-inp = input("Enter Input : ").split(',')
-new_stack_list = []
+stack_input = input("Enter Input : ").split(",")
 
-for i in inp:
+for i in stack_input:
     if i[0] == "A":
         stack.push(i[2:])
-        print("Add = " + i[2:])
+        print("Add = " + str(i[2:]))
     elif i[0] == "P":
         if not stack.is_empty():
             print("Pop = " + stack.pop())
         else:
-            print("-1")
+            print(-1)
     elif i[0] == "D":
         if not stack.is_empty():
             for j in range(stack.size()):
                 last_items = stack.pop()
-                if last_items != i[2:]:
-                    new_stack_list.append(last_items)
+                if int(last_items) != int(i[2:]):
+                    new_list_stack.push(last_items)
                 else:
-                    print("Delete = " + str(last_items))
+                    print("Delete : " + str(last_items))
         else:
-            print("-1")
-    elif i[0] == 'L' and i[1] == 'D':
+            print(-1)
+    elif i[0] == "L" and i[1] == "D":
         if not stack.is_empty():
             for j in range(stack.size()):
                 last_items = stack.pop()
                 if int(last_items) >= int(i[2:]):
-                    new_stack_list.append(last_items)
+                    new_list_stack.push(last_items)
                 else:
                     print("Delete = " + str(last_items) + " Because " + str(last_items) + " is less than" + str(i[2:]))
         else:
-            print("-1")
-    elif i[0] == 'M' and i[1] == 'D':
+            print(-1)
+    elif i[0] == "M" and i[1] == "D":
         if not stack.is_empty():
             for j in range(stack.size()):
                 last_items = stack.pop()
                 if int(last_items) <= int(i[2:]):
-                    new_stack_list.append(last_items)
+                    new_list_stack.push(last_items)
                 else:
                     print("Delete = " + str(last_items) + " Because " + str(last_items) + " is more than" + str(i[2:]))
         else:
-            print("-1")
+            print(-1)
     
-    for k in range(len(new_stack_list)):
-        stack.push(new_stack_list.pop())
+    for k in range(new_list_stack.size()):
+        stack.push(new_list_stack.pop())
 
 if stack.is_empty():
     print("Value in Stack = []")
