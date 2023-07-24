c52ff574a1ac771aab142cbb6cbe2f59d810a5f1
khris-xp <khrisbharmmano@gmail.com>
2023-07-24 22:47:29 +0700

    refac: ex03 stack

--- Stack/ex03.py
+++ Stack/ex03.py
@@ -1,50 +1,46 @@
 class Stack:
-    def __init__(self, operations, priority, stack, output, expression):
-        self.operations = operations
-        self.priority = priority
-        self.stack = stack
-        self.output = output
-        self.expression = expression
-    
-    def push(self,value):
-        self.stack.append(value)
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
 
     def pop(self):
-        self.stack.pop()
+        return self.stack.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return None
 
     def size(self):
         return len(self.stack)
 
-    def isEmpty(self):
-        return self.size() == 0
+    def is_empty(self):
+        return len(self.stack) == 0
 
 inp = input("Enter Infix : ")
 
-stack = Stack(None, None, None, None, None)
-
-stack.operations = set(['+', '-', '*', '/', '(', ')', '^'])
-stack.priority = {'+':1, '-':1, '*':2, '/':2, '^':3, '(':0}
-stack.stack = []
-stack.output = ""
+stack = Stack()
+operations = set(['+', '-', '*', '/', '^', '(', ')'])
+priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '(': 0}
+output = ""
 
 for i in inp:
-    if i not in stack.operations:
-        stack.output += i
+    if i not in operations:
+        output += i
     elif i == '(':
         stack.push(i)
     elif i == ')':
-        while stack.size() > 0 and stack.stack[-1] != '(':
-            stack.output += stack.stack[-1]
-            stack.pop()
+        while stack.size() > 0 and stack.peek() != '(':
+            output += stack.pop()
         stack.pop()
     else:
-        while stack.size() > 0 and stack.priority[i] <= stack.priority[stack.stack[-1]]:
-            stack.output += stack.stack[-1]
-            stack.pop()
+        while stack.size() > 0 and priority[i] <= priority[stack.peek()]:
+            output += stack.pop()
         stack.push(i)
 
 while stack.size() > 0:
-    stack.output += stack.stack[-1]
-    stack.pop()
+    output += stack.pop()
 
-print("Postfix : " + stack.output)
+print("Postfix : " + output)
