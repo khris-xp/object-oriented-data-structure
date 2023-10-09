548b54546501bd65cb40502a53b0e355e5a65c7d
khris-xp <khrisbharmmano@gmail.com>
2023-10-09 22:11:37 +0700

    refac: ex02 searching

--- Searching/ex02.py
+++ Searching/ex02.py
@@ -1,17 +1,20 @@
-data = [int(e) for e in input('Data : ').split()]
-res = 0
-index = seq = 1
-stack = []
+def check_longest_increasing_subsequence(data):
+    res = 0
+    index = seq = 1
+    stack = []
 
-for n in data:
-    if stack:
-        while stack[-1] >= n:
-            stack.pop(-1)
-            if not stack:
-                break
-    stack.append(n)
-    print(index, stack, sep=" : ")
-    res = max(res, len(stack))
-    index += 1
+    for n in data:
+        if stack:
+            while stack[-1] >= n:
+                stack.pop(-1)
+                if not stack:
+                    break
+        stack.append(n)
+        print(index, stack, sep=" : ")
+        res = max(res, len(stack))
+        index += 1
 
-print('longest increasing subsequence :', res)
+    return res
+
+data_input = [int(e) for e in input('Data : ').split()]
+print('longest increasing subsequence :', check_longest_increasing_subsequence(data_input))
